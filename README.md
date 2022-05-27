2336173254dee093d46bec035dcbdb5f9766aa10
Kenny John Jacob <johnjacobkenny@gmail.com>
2022-05-27 19:36:48 +0530

    Refactor code

--- README.md
+++ README.md
@@ -1,51 +1,16 @@
-<img src="https://avatars2.githubusercontent.com/u/2810941?v=3&s=96" alt="Google Cloud Platform logo" title="Google Cloud Platform" align="right" height="96" width="96"/>
+# FactoHR Punch In Automation using Puppeteer and Google Cloud
 
-# Google Cloud Functions - Hello World sample
-"puppeteer-extra": "^3.1.9",
+This project is used to automate the checkin flow in FactoHR. This is required because of the bad UX requiring 3 or 4 clicks to checkin.
+
+## Publish to google cloud
+`npm run publish`
+
+## Future
+Take a look at these packages for future,
+```
+   "puppeteer-extra": "^3.1.9",
    "puppeteer-extra-plugin-adblocker": "^2.11.3",
    "puppeteer-extra-plugin-anonymize-ua": "^2.2.8",
    "puppeteer-extra-plugin-block-resources": "^2.2.4",
    "puppeteer-extra-plugin-stealth": "^2.4.9"
-Seehttps://github.com/alixaxel/chrome-aws-lambda:
-
-`gcloud beta functions deploy factohr-checkin --gen2 --runtime nodejs16 --trigger-http --entry-point factoHRCheckin --source . - -allow-unauthenticated --memory=1024MB`
-
-* [Cloud Functions Hello World tutorial][tutorial]
-* [Cloud Functions Hello World sample source code][code]
-
-[tutorial]: https://cloud.google.com/functions/docs/quickstart
-[code]: index.js
-
-## Deploy and run the sample
-
-See the [Cloud Functions Hello World tutorial][tutorial].
-
-**Note:** in order for the tests to run properly, you'll have to deploy some of the sample functions:
-
 ```
-gcloud functions deploy helloHttp --runtime [YOUR_RUNTIME] --trigger-http
-gcloud functions deploy helloPubSub --trigger-topic $FUNCTIONS_TOPIC --runtime [YOUR_RUNTIME]
-gcloud functions deploy helloGCS --runtime [YOUR_RUNTIME] --trigger-resource $FUNCTIONS_DELETABLE_BUCKET --trigger-event providers/cloud.storage/eventTypes/object.change
-```
-
-* Replace `[YOUR_RUNTIME]` with the name of the runtime you are using. For a
-complete list, see the [gcloud reference](https://cloud.google.com/sdk/gcloud/reference/functions/deploy#--runtime).
-
-## Run the tests
-
-1. Read and follow the [prerequisites](../../../../#prerequisites).
-
-
-1. Install dependencies:
-
-        npm install
-
-1. Set the following environment variables:
-
-        export GCF_REGION=us-central1
-        export FUNCTIONS_TOPIC=[YOUR_PUBSUB_TOPIC]
-        export FUNCTIONS_DELETABLE_BUCKET=[YOUR_CLOUD_STORAGE_BUCKET]  # will be deleted by tests!
-
-1. Run the tests:
-
-        npm test
--- index.js
+++ index.js
@@ -1,99 +1,31 @@
 'use strict';
 
 /* eslint-disable no-unused-vars */
-
+const { chrome_args } = require("./chrome")
 const functions = require('@google-cloud/functions-framework');
 const p = require("puppeteer");
+const { login, punchIn } = require("./checkin")
 
-/**
- * Responds to an HTTP request using data from the request body parsed according
- * to the "content-type" header.
- *
- * @param {Object} req Cloud Function request context.
- * @param {Object} res Cloud Function response context.
- */
+
 functions.http('factoHRCheckin', async (req, res) => {
-  const minimal_args = [
-  '--autoplay-policy=user-gesture-required',
-  '--disable-background-networking',
-  '--disable-background-timer-throttling',
-  '--disable-backgrounding-occluded-windows',
-  '--disable-breakpad',
-  '--disable-client-side-phishing-detection',
-  '--disable-component-update',
-  '--disable-default-apps',
-  '--disable-dev-shm-usage',
-  '--disable-domain-reliability',
-  '--disable-extensions',
-  '--disable-features=AudioServiceOutOfProcess',
-  '--disable-gpu',
-  '--disable-hang-monitor',
-  '--disable-ipc-flooding-protection',
-  '--disable-notifications',
-  '--disable-offer-store-unmasked-wallet-cards',
-  '--disable-popup-blocking',
-  '--disable-print-preview',
-  '--disable-prompt-on-repost',
-  '--disable-renderer-backgrounding',
-  '--disable-setuid-sandbox',
-  '--disable-speech-api',
-  '--disable-sync',
-  '--hide-scrollbars',
-  '--ignore-gpu-blacklist',
-  '--metrics-recording-only',
-  '--mute-audio',
-  '--no-default-browser-check',
-  '--no-first-run',
-  '--no-pings',
-  '--no-sandbox',
-  '--no-zygote',
-  '--password-store=basic',
-  '--use-gl=swiftshader',
-  '--use-mock-keychain',
-];
-  let message = "";
+  let response = null;
   const username = req.body.username;
   const password = req.body.password;
 
   if (username == null || password == null) {
-    return res.send("Please enter username or password.") 
+    return res.send("Please enter username or password.")
   }
 
   try {
-    const usernameSelector = "#txtUsername";
-    const passwordSelector = "#txtPassword";
-
-    const attendanceSelector = '[data-module-name="Attendance"]';
-    const browser = await p.launch({args: minimal_args})
+    const browser = await p.launch({ args: chrome_args })
     const page = await browser.newPage()
 
-    await page.goto("https://app2.factohr.com/matter/Security/Login")
-    await page.waitForTimeout(1000)
-
-    await page.waitForSelector(usernameSelector)
-    await page.waitForSelector(passwordSelector)
-    await page.click(usernameSelector)
-    await page.keyboard.type(username)
-
-    await page.click(passwordSelector)
-    await page.keyboard.type(password)
-
-    await page.click("#btnLogin")
-    await page.waitForNavigation()
-
-    await page.goto("https://app2.factohr.com/matter/site/Attendance/OnlineAttendance.aspx")
-    const checkIn = await page.waitForSelector("#ctl00_CPH_btnCheck")
-    await checkIn?.click()
-
-    const checkInMessage = await page.waitForSelector("#messagebox-1001-body")
-    if (checkInMessage) {
-      message = await checkInMessage.evaluate(el => el.textContent)
-    }
-
+    await login(page)
+    response = await punchIn(page)
     await page.close()
   }
   catch (err) {
     console.log(err)
   }
-  res.send(message);
+  res.send(response);
 });
